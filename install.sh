38aaadaa9bf7a444620af12d52fec813b5adcc86
Skyler Lehan <slehan117@icloud.com>
2020-02-02 11:28:45 -0500

    Add unattended install for oh-my-zsh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ echo "Setting up your vimrc..."
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 echo "Installing zshrc and oh-my-zsh along with some plugins..."
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 touch ~/.zshrc-custom
