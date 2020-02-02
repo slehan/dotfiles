18d29ce7e6ae4504dfcdd729317b49c0197c207e
Skyler Lehan <slehan117@icloud.com>
2020-02-02 11:58:08 -0500

    Add statement to change shell to zsh

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@ echo "Setting up your vimrc..."
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 echo "Installing zshrc and oh-my-zsh along with some plugins..."
-RUNZSH='no'
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 touch ~/.zshrc-custom
+echo "Changing shell to zsh"
+chsh -s $(which zsh)
 echo "To customize your .zshrc, edit the ~/.zshrc-custom file"
 
 echo "Setting up tmux..."
