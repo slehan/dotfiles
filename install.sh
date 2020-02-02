74350dc79c87d3181593113a3a936ad9cd4be060
Skyler Lehan <212836+slehan@users.noreply.github.com>
2020-02-02 11:59:37 -0500

    Resolve issues installing

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@ echo "Setting up your vimrc..."
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 echo "Installing zshrc and oh-my-zsh along with some plugins..."
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 touch ~/.zshrc-custom
+echo "Changing shell to zsh"
+chsh -s $(which zsh)
 echo "To customize your .zshrc, edit the ~/.zshrc-custom file"
 
 echo "Setting up tmux..."
-ln -s -f ~/.dotfiles/.tmux/.tmux.conf ~/.tmux/.tmux.conf
+git submodule update --init
+mkdir ~/.tmux
+ln -s ~/.dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux/.tmux.conf.local ~/.tmux.conf.local
 echo "If you want to customize tmux, edit ~/.tmux.conf.local"
